27b0e83895acc0d2735270dea7f14292bd63a7e6
cungquang <cungquang1987@gmail.com>
2024-02-18 13:20:09 -0800

    test case

--- app/src/main.c
+++ app/src/main.c
@@ -23,30 +23,21 @@ int main()
 {	
 	char buffer[1500];
 	int buffer_size = 0;
-	double test = 1234.22343;
-	double test2 = 4342.23123;
-	double test3 = 7548.23411;
-	int char_size = 0;
-	int char_size2 = 0;
-	int char_size3 = 0;
-	const char *str1 = convertDataToString(&char_size, test);
-	const char *str2 = convertDataToString(&char_size2, test2);
-	const char *str3 = convertDataToString(&char_size3, test3);
-	
-	mergeToBuffer(buffer, &buffer_size, str1, char_size);
-	printf("%d - %s\n", buffer_size, buffer);
+	double strNum[] = {123.3425, 3431.1234, 23123.3416};
 
-	mergeToBuffer(buffer, &buffer_size, str2, char_size2);
-	printf("%d - %s\n", buffer_size, buffer);
+	for(int i = 0; i < 3; i++)
+	{
+		int char_size;
+		const char *str = convertDataToString(&char_size, strNum[i]);
+		
+		//merge to buffer
+		mergeToBuffer(buffer, &buffer_size, str, char_size);
+		printf("%d - %s\n", buffer_size, buffer);
 
-	mergeToBuffer(buffer, &buffer_size, str3, char_size3);
-	printf("%d - %s\n", buffer_size, buffer);
+		//free memory for the next round
+		free((void *) str);
+		str = NULL;
+	}
 
-	free((void *)str1);
-	free((void *)str2);
-	free((void *)str3);
-	str1 = NULL;
-	str2 = NULL;
-	str3 = NULL;
 	return 0;
 }
